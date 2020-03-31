ba0351f2cc8d6ee8c248806f50f5d52aef0a5df4
Mitico-S <Mitico-S@users.noreply.github.com>
2020-03-31 10:30:31 -0300

    atualização de menu

---
+++ Exercicios/js/menu.js
@@ -0,0 +1,9 @@
+function menu () {
+    var barraMenu = document ("<nav class="navbar navbar-expand-lg navbar-light"><button class="navbar-toggler" type="button" data-toggle="collapse"data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button><div class="collapse navbar-collapse main-menu-item justify-content-center"id="navbarSupportedContent"><ul class="navbar-nav align-items-center"><li class="nav-item"><a class="nav-link" href="index.html">Home</a></li><li class="nav-item"><a class="nav-link" href="Sobre mim.html">Sobre mim</a></li><li class="nav-item"><a class="nav-link" href="Estudo.html">Material de Estudos</a> </li><li class="nav-item dropdown"><a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Materias 2º Semestre</a><div class="dropdown-menu" aria-labelledby="navbarDropdown"><a class="dropdown-item" href="Engenharia de software.html">Engenharia de Software</a><a class="dropdown-item" href="Js.html">Aplicações Web</a><a class="dropdown-item" href="Sistemas Operacionais.html">Sistemas Operacionais</a><a class="dropdown-item" href="Banco de dados.html">Banco de dados</a><a class="dropdown-item" href="Algoritimos.html">Algorítimos</a></div></li><li class="nav-item"><a class="nav-link" href="Programas.html">Programas</a></li></ul></div></nav>")
+
+
+
+
+
+}
+
